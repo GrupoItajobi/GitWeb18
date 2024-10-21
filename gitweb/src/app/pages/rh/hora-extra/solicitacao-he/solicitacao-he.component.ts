be57f0210c70b99dea0e610b9164ca31e6c4e191
JulioCBressanin <97546202+JulioCBressanin@users.noreply.github.com>
2024-10-21 15:02:48 -0300

    Solicitacao Hora Extra

---
+++ gitweb/src/app/pages/rh/hora-extra/solicitacao-he/solicitacao-he.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitacao-he',
+  standalone: true,
+  imports: [],
+  templateUrl: './solicitacao-he.component.html',
+  styleUrl: './solicitacao-he.component.scss'
+})
+export class SolicitacaoHeComponent {
+
+}
--- gitweb/src/environments/environment.ts
+++ gitweb/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  versaoApp: '2024.10.16 12:15',
+  versaoApp: '2024.10.17 12:11',
   apiUrl: 'https://git-api.usinaitajobi.com.br:8443/',
   // apiUrl: 'https://spring.gvo.com.br:8080/',
   licencaApp: '018d314d-1014-757a-b087-f96c3f3ac385'
