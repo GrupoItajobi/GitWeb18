b6215119212e7718a1579c871fbf9c0092d4a731
JulioCBressanin <97546202+JulioCBressanin@users.noreply.github.com>
2024-10-28 15:15:14 -0300

    ButtonCard

--- gitweb/src/app/pages/rh/hora-extra/aprovacao-solicitacao-he/aprovacao-solicitacao-he.component.ts
+++ gitweb/src/app/pages/rh/hora-extra/aprovacao-solicitacao-he/aprovacao-solicitacao-he.component.ts
@@ -5,10 +5,11 @@ import { TableModule } from 'primeng/table';
 import { HoraExtraService } from '../../../../services/rh/hora-extra/hora-extra.service';
 import { minutosEmHorasStr } from '../../../../core/util/gitweb-util';
 import { CommonModule } from '@angular/common';
+import { ButtonCardComponent, ButtonCardOptions } from "../../../../components/button/button-card/button-card.component";
 @Component({
   selector: 'app-aprovacao-solicitacao-he',
   standalone: true,
-  imports: [CommonModule, TableModule],
+  imports: [CommonModule, TableModule, ButtonCardComponent],
   templateUrl: './aprovacao-solicitacao-he.component.html',
   styleUrl: './aprovacao-solicitacao-he.component.scss'
 })
@@ -17,6 +18,7 @@ export class AprovacaoSolicitacaoHeComponent implements OnInit {
   solicitacoes: SolicitacaoHoraExtraPorAprovador[] = [];
   solicitacaoSelecionada: SolicitacaoHoraExtraPorAprovador = {};
 
+  optionsButtonCard: ButtonCardOptions[] = [];
   constructor(
     private horaExtraService: HoraExtraService,
     private errorHandleService: ErrorHandleService,
@@ -26,6 +28,7 @@ export class AprovacaoSolicitacaoHeComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.loadOptionButtonCard();
     this.init();
   }
 
@@ -39,8 +42,29 @@ export class AprovacaoSolicitacaoHeComponent implements OnInit {
       })
 
   }
-
-  minutosEmHoras(minutos:number=0):string {
+  loadOptionButtonCard() {
+    this.optionsButtonCard = [
+      {
+        img: "/assets/layout/img/sistema/aprovado.png",
+        title: "Aprovar",
+        clicked: false,
+        returnWhenClicked: "aprovado"
+      },
+      {
+        img: "/assets/layout/img/sistema/reprovado.png",
+        title: "Reprovar",
+        clicked: false,
+        returnWhenClicked: "reprovado"
+      }
+    ];
+  }
+  minutosEmHoras(minutos: number = 0): string {
     return minutosEmHorasStr(minutos)
   }
+
+  clickButtonCard(event: string) {
+    console.log('clickButtonCard: '+event);
+  }
+
+
 }
